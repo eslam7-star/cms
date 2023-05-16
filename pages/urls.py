b753b886c4eec7926748b1788b3cce2819a0816d
Siveen said <21p0149@eng.asu.edu.eg>
2023-05-16 20:36:56 +0300

    function add and delete appointments

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from pages.views import delete_user,create_appointment
 
 urlpatterns = [
 
-    path('', views.index, name='index'),
+     path('', views.index, name='index'),
     path('login_register/', views.login_register, name='login'),
+   path('user', views.user, name='user' ),
+   path('delete_user/',delete_user,name='delete_user'),
+    path('create-appointment/', create_appointment, name='create_appointment'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from pages.models import Appointment
+
 
 # Create your views here.
 
@@ -7,4 +11,34 @@ def index(request):
 
 def login_register(request):
     return render(request, 'login_register.html')
+def user(request):
+    return render(request, 'user.html')
+def delete_user(request):
+    if request.method == 'DELETE':
+        user_id = request.GET.get('user_id')
+        appointment = get_object_or_404(Appointment, id=user_id)
+        appointment.delete()
+        return JsonResponse({'status': 'success'})
+def create_appointment(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phonenum = request.POST.get('phonenum')
+        telephonenum = request.POST.get('telephonenum')
+        date = request.POST.get('date')
+        appointment_id = request.POST.get('Id') #3shan 7war el app
+        if Appointment.objects.filter(Id=appointment_id).exists():
+            return JsonResponse({'success': False, 'message': 'An appointment with this ID already exists change the id'})
+        # Otherwise, create a new appointment object and save it to the database
+        appointment = Appointment(name=name, email=email, phonenum=phonenum,
+                                  telephonenum=telephonenum, date=date, Id=appointment_id)
+        appointment.save()
+        return JsonResponse({'success': True, 'message': 'Appointment created successfully'})
+    else:
+        return render(request, 'user.html')
+
+  
+
+
+
 
